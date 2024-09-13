f8a834d2863c91f8de396f8a1088d2718abce2d7
brendonshuke <brendon@dev.al>
2024-09-13 18:15:20 -0400

    initial commit

---
+++ src/main/java/com/bshuke/java_multi_threading/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bshuke.java_multi_threading;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaMultiThreadingApplication.class);
+	}
+
+}
---
+++ src/test/java/com/bshuke/java_multi_threading/JavaMultiThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bshuke.java_multi_threading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultiThreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
